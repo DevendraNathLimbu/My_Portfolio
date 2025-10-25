33ca30c457ecdd7ba6890c9d8f93fdc0f89b4f93
DevendraNathLimbu <srijansilimbolimbu@gmail.com>
2025-10-25 10:43:34 +0545

    port change

--- backend/script.js
+++ backend/script.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = 8080;
+const port = 5000;
 const path = require('path');
 const dotenv = require('dotenv');
 dotenv.config();
